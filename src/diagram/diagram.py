c36fe7e7dc5ca9b06534c0a491aa89a2cc776ad4
Patrick Morton <patrick.morton.-nd@disney.com>
2023-09-17 10:29:44 -0400

    diagram

---
+++ src/diagram/diagram.py
@@ -0,0 +1,94 @@
+import weather_api
+
+MERMAID_FILE = 'weather_diagram.md'
+MERMAID_HEADER = "```mermaid\ngraph LR;"
+
+
+def write_mermaid_md(mermaid_data):
+    """
+    Writes the mermaid data to a file in the local folder
+    
+    Args:
+        mermaid_data (list) : List containing the mermaid markdown
+        
+    Returns:
+        None
+    """
+    try:
+        with open(MERMAID_FILE, 'w') as fp:
+            fp.write("%s\n" % MERMAID_HEADER)
+            for _ in mermaid_data:
+                fp.write("    %s\n" % _)
+    
+    except Exception as e:
+        print(f"Failed to write file to disk. error is {e}")
+
+
+
+def generate_mermaid_markdown(zip_code, country_code = None): 
+    """
+    Uses the weather_api to get weather data for the given zip code
+    and generates the mermaid markdown and writes it to a file in the local folder
+    
+    Args:
+        zip_code (str) : The zip code
+        
+    Returns:
+        None
+    """
+    location_info = weather_api.get_location(zip_code, country_code)
+    lat = location_info.get('lat')
+    lon = location_info.get('lon')
+    data = weather_api.get_weather_forecast(lat = lat, lon = lon)
+
+    mermaid_md = []
+
+    for parent, children in data.items():
+        get_children(children, parent, mermaid_md)
+
+    write_mermaid_md(mermaid_md)
+
+
+def get_children(children, parent: str, mermaid_md):
+    """
+    Helper function to extract parent child relationship 
+    from the provided JSON data which is then used to generate 
+    a mermaid diagram.
+
+    This function uses recursion to get the data from all nodes.
+    
+    Args:
+        children (dict | list | str) : data to be extracted
+        parent (str) : This is the parent value of the node relationship 
+        mermaid_md (list) : This holds the extracted data
+
+    Returns:
+        None
+    """
+    if isinstance(children, dict):
+        for k, v in children.items():
+            chain = f"{parent}-->"
+            chain += f"{parent}_{k}-->"
+            if isinstance(v, dict):
+                get_children(v, chain)
+            else:
+                if isinstance(v, list):
+                    counter = 0
+                    for _ in v:
+                        get_children(_, chain)
+                        counter +=1
+                else:
+                    v = str(v).replace(' ','_')  # replace spaces with underscore to prevent syntax errors on render
+                    chain += f"{v};"
+                    mermaid_md.append(chain)
+    else:
+        if isinstance(children, list):
+            counter = 0
+            for _ in children:
+                parent =f"{parent}_{counter}"
+                get_children(_, parent, mermaid_md)
+        else:
+            children = str(children).replace(' ','_')  # replace spaces with underscore to prevent syntax errors on render
+            chain = f"{parent}-->"
+            chain += f"{children};"
+            mermaid_md.append(chain)
